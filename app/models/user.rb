15f0b2e482f1e987a24bcfe4fa23f6ba3b0cfec3
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-09 10:15:29 +0300

    Added model for the user

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    # app/models/user.rb
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
+
+    def authenticate(password)
+        if BCrypt::Password.new(self.password_digest) == password
+            self
+        else
+            false
+        end
+    end
+end
